9fae5b7c6016ce436939a57a98a8dba585b44166
user <user@user-PC>
2022-12-07 23:46:12 +0530

    leadcommits

---
+++ leadproject/src/generic_scripts/Base_Class.java
@@ -0,0 +1,31 @@
+package generic_scripts;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base_Class implements Framework_constants {
+	public WebDriver driver;
+    @BeforeMethod
+    public void openapp() 
+    {
+    	System.setProperty(gecko_key,gecko_value);
+        driver=new FirefoxDriver();
+        driver.get(url);   
+    }
+    
+    @AfterMethod
+    public void close(ITestResult result) throws IOException
+    {
+    	if(ITestResult.FAILURE==result.getStatus())
+    	{
+    		Generic_Photo.getPhoto(driver);
+    	}
+		driver.close();
+    }
+}
+
---
+++ leadproject/src/generic_scripts/Generic_Excel.java
@@ -0,0 +1,24 @@
+package generic_scripts;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Generic_Excel {
+
+	
+	public static String getdata(String sheet,int row,int cell) throws EncryptedDocumentException, IOException
+	{
+		FileInputStream fis=new FileInputStream("./excel/Test.xlsx");
+		Workbook book = WorkbookFactory.create(fis);
+		Cell cel = book.getSheet(sheet).getRow(row).getCell(cell);
+		String value = cel.toString();
+		return value;
+	}
+	
+	    
+}
